07b3f833671a9f14486250e5c7e9ad606e8a6971
ak-51 <aditya.kumar.752cz@gmail.com>
2021-07-05 18:05:01 +0800

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,26 @@
-import express from 'express'
-import cors from 'cors'
 import mongoose from 'mongoose'
+import app from './server.js'
+dotenv.config()
+const port = process.env.PORT || 8000
+const url = process.env.DB_URI
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const noteSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    favnum: Number
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const nt1 = new Note({
+    title: "2nd note",
+    content: "im learning react + mongoose",
+    favnum: 75
+})
+
+nt1.save().then(result => {
+    console.log('note saved')
+    mongoose.connection.close()
+})
