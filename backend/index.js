cd057c7ef24ec87df5883588f2897da8c37c8fcc
ak-51 <aditya.kumar.752cz@gmail.com>
2021-07-05 18:28:36 +0800

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
+import express from 'express'
 import mongoose from 'mongoose'
-import app from './server.js'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
 dotenv.config()
 const port = process.env.PORT || 8000
 const url = process.env.DB_URI
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
@@ -14,6 +25,14 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
+app.get('/api/notes', (req, res) =>{
+    Note.find({}).then(notes => {
+        res.json(notes)
+        mongoose.connection.close()
+    })
+})
+
+```
 const nt1 = new Note({
     title: "2nd note",
     content: "im learning react + mongoose",
@@ -24,3 +43,4 @@ nt1.save().then(result => {
     console.log('note saved')
     mongoose.connection.close()
 })
+```
